0ad7d75c8df7bf95cc1a140ee1d61218faed0404
lucasbastida <48137388+lucasbastida@users.noreply.github.com>
2023-09-26 22:34:38 -0300

    Configure testcontainers

--- src/test/java/com/example/salariogod/SalariogodApplicationTests.java
+++ src/test/java/com/example/salariogod/SalariogodApplicationTests.java
@@ -2,12 +2,24 @@ package com.example.salariogod;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 @SpringBootTest
+@Testcontainers
 class SalariogodApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-bookworm");
 
 	@Test
 	void contextLoads() {
+		assertThatCode(() -> {
+		}).doesNotThrowAnyException();
 	}
-
 }
